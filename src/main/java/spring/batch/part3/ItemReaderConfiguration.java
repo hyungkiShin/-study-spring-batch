d319342b1557c1f60731d041b54f23d8a40accfc
hyungkiShin <tkaqkeldk@nate.com>
2023-01-01 23:38:59 +0900

    feat: JdbcItemReader 중 Paging 기반 ItemReader 구현체를 사용

--- src/main/java/spring/batch/part3/ItemReaderConfiguration.java
+++ src/main/java/spring/batch/part3/ItemReaderConfiguration.java
@@ -8,7 +8,12 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.PagingQueryProvider;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -16,10 +21,13 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Configuration
@@ -44,7 +52,8 @@ public class ItemReaderConfiguration {
                 .incrementer(new RunIdIncrementer())
                 .start(this.customItemReaderStep())
                 .next(this.csvFileStep())
-                .next(this.jdbcStep())
+                .next(this.jdbcCursorStep())
+                .next(this.jdbcPagingStep())
                 .build();
     }
 
@@ -67,10 +76,19 @@ public class ItemReaderConfiguration {
     }
 
     @Bean
-    public Step jdbcStep() throws Exception {
-        return this.stepBuilderFactory.get("jdbcStep")
+    public Step jdbcCursorStep() throws Exception {
+        return this.stepBuilderFactory.get("jdbcCursorStep")
                 .<Person, Person>chunk(10)
                 .reader(jdbcCursorItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcPagingStep() throws Exception {
+        return this.stepBuilderFactory.get("jdbcPagingStep")
+                .<Person, Person>chunk(10)
+                .reader(jdbcPagingItemReader())
                 .writer(itemWriter())
                 .build();
     }
@@ -86,6 +104,36 @@ public class ItemReaderConfiguration {
 
         itemReader.afterPropertiesSet(); // 여기서 afterPropertiesSet 이 하는 일은 각각의 Writer 들이 실행되기 위해 필요한 필수값들이 제대로 세팅되어있는지를 체크합니다.
         return itemReader;
+    }
+
+    private JdbcPagingItemReader<Person> jdbcPagingItemReader() throws Exception {
+
+        final JdbcPagingItemReader<Person> itemReader = new JdbcPagingItemReaderBuilder<Person>()
+                .name("jdbcPagingItemReader")
+                .dataSource(dataSource)
+                .pageSize(10)
+                .fetchSize(10)
+                .rowMapper((rs, rowNum) -> new Person( // column index 는 0 번 부터가 아닌 1번부터 시작한다.
+                        rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4))) // 조회된 데이터를 Person 객체에 매핑
+                .queryProvider(createQueryProvider())
+                .build();
+
+        itemReader.afterPropertiesSet();
+        return itemReader;
+    }
+
+    public PagingQueryProvider createQueryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+        queryProvider.setDataSource(dataSource);
+        queryProvider.setSelectClause("id, name, age, address");
+        queryProvider.setFromClause("from person");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        return queryProvider.getObject();
     }
 
     private FlatFileItemReader<Person> csvFileItemReader() throws Exception {
